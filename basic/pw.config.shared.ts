232ba62d424bf7edd791571056cc161f37880acd
DJ Mountney <dj@currents.dev>
2025-06-05 15:30:16 -0700

    chore: based on workers

--- basic/pw.config.shared.ts
+++ basic/pw.config.shared.ts
@@ -12,7 +12,7 @@ const config = defineConfig<CurrentsFixtures, CurrentsWorkerFixtures>({
 
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : undefined,
 
   use: {
     actionTimeout: 0,
